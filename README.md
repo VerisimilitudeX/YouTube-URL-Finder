c5be38308dd014a1238f78ceda07bc1b852e1b83
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-04 18:14:15 -0700

    Added Limitations

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # YouTube URL Finder
- This software analyzes webpages for YouTube URLs and returns them.
+This software analyzes webpages for YouTube URLs and returns them.
+## Known Limitations
+- YouTube URLs are not found in the HTML code of the webpage.
+- The website denies access to the YouTube API (403 Forbidden).
---
+++ YouTube-URL-Finder.java
@@ -0,0 +1,28 @@
+// This class provides methods for accessing a web page. You can create a URLResource by giving it a complete 
+// URL, or web address (note, it must start with either http:// or https://):
+import edu.duke.URLResource;
+import java.util.Scanner;
+
+// This class finds all the YouTube videos in a web page and returns the URLs of the videos.
+public class Part4 {
+    public static void returnYouTubeLinks() {
+        Scanner ur = new Scanner(System.in);
+        System.out.println("Enter the URL of the document you want to search within: ");
+        String URL = ur.nextLine();
+        ur.close();
+        System.out.println("----------------------------------------------------");
+        URLResource resource = new URLResource(URL);
+        for (String word : resource.words()) {
+            String wordl = word.toLowerCase();
+            int index = wordl.indexOf("youtube.com");
+            if (index != -1) {
+                int start = word.indexOf("\"", index - 13);
+                int stop = word.indexOf("\"", index);
+                System.out.println(word.substring(start + 1, stop));
+            }
+        }
+    }
+    public static void main(String[] args) {
+         returnYouTubeLinks();
+    }
+}
